6c6b078936a106bb73a743d26b45e7b7c9aba808
Marshall <mmoutenot@gmail.com>
2013-04-30 20:55:00 -0400

    rewrite ajax for upload

--- static/javascripts/upload.js
+++ static/javascripts/upload.js
@@ -1,54 +1,10 @@
 $(document).ready(function() {
-  // $('#uploadModalContent').load('uploadModal.html');
-  // console.log('loaded upload modal');
-});
-
-$(document).ready(function() {
   $('#uploadButton').click(function(e) {
     console.log('upload clicked');
     e.preventDefault();
     $('#uploadModal').reveal();
   });
 });
-
-function formSubmit()
-{
-  url = document.getElementById("url").value;
-  link = '/image/?url=' + url;
-  $("#images").html("");
-  $.getJSON(link, function(data) {
-    $.each(data, function(i,item){
-      $("<img/>").attr("src", item).attr('class', 'selectable-image').appendTo("#images");
-    });
-    $(".selectable-image").click(function(event) {
-      $(".selected-image").removeClass('selected-image');
-      $(this).addClass('selected-image');
-    });
-    $('.tags-input').show();
-    $('#error-message').hide();
-    document.getElementById('error-message').innerHTML = "";
-
-    $("#select-button").unbind('click');
-    $("#select-button").click(function() {
-      var image_url = $('.selected-image').attr('src');
-      var tags = document.getElementById('tag').value.split(" ");
-      $.ajax({
-        url: "/add_item/",
-        type: 'PUT',
-        data: {board_id: boardId, url: url,
-          image_url: image_url, tags: tags, pos_x: 100, pos_y: 200,
-        scale: 1, locked: true
-        }}).done(function() {
-          $("#add-image-modal").trigger('reveal:close');
-        });
-    });
-  })
-  .error(function() {
-    document.getElementById('error-message').innerHTML = "Image not found!";
-    $('.tags-input').hide();
-    $('#error-message').show();
-  });
-}
 
 $(document).ready(function() {
   var holder = document.getElementById('holder'),
@@ -60,13 +16,13 @@ $(document).ready(function() {
   },
   support = {
     filereader: document.getElementById('filereader'),
-    formdata: document.getElementById('formdata'),
-    progress: document.getElementById('progress')
+  formdata: document.getElementById('formdata'),
+  progress: document.getElementById('progress')
   },
   acceptedTypes = {
     'image/png': true,
-    'image/jpeg': true,
-    'image/gif': true
+  'image/jpeg': true,
+  'image/gif': true
   },
   progress = document.getElementById('uploadprogress'),
   fileupload = document.getElementById('upload');
@@ -75,7 +31,6 @@ $(document).ready(function() {
     if (tests[api] === false) {
       support[api].className = 'fail';
     } else {
-      console.log('hiding '+api);
       // FFS. I could have done el.hidden = true, but IE doesn't support
       // hidden, so I tried to create a polyfill that would extend the
       // Element.prototype, but then IE10 doesn't even give me access
@@ -83,6 +38,7 @@ $(document).ready(function() {
       support[api].className = 'hidden';
     }
   });
+
 
   function previewfile(file) {
     if (tests.filereader === true && acceptedTypes[file.type] === true) {
@@ -102,44 +58,42 @@ $(document).ready(function() {
   }
 
   function readfiles(files) {
-    debugger;
-    var formData = tests.formdata ? new FormData() : null;
-    for (var i = 0; i < files.length; i++) {
-      if (tests.formdata) formData.append('file', files[i]);
-      previewfile(files[i]);
-    }
+      var formData = tests.formdata ? new FormData() : null;
+      for (var i = 0; i < files.length; i++) {
+        if (tests.formdata) formData.append('file', files[i]);
+        previewfile(files[i]);
+      }
 
-    if(tests.formdata){
-      $.ajax({
-        xhr: function()
-        {
-          var xhr = new window.XMLHttpRequest();
-          //Upload progress
-          xhr.upload.addEventListener("progress", function(evt){
-            if (evt.lengthComputable) {
-              var percentComplete = evt.loaded / evt.total;
-              //Do something with upload progress
-              console.log(percentComplete);
-            }
-          }, false);
-          //Download progress
-          xhr.addEventListener("progress", function(event){
+      // now post a new XHR request
+      if (tests.formdata) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', '/space/upload/');
+        xhr.onload = function(response) {
+          progress.value = progress.innerHTML = 100;
+        };
+
+        if (tests.progress) {
+          xhr.upload.onprogress = function (event) {
             if (event.lengthComputable) {
               var complete = (event.loaded / event.total * 100 | 0);
               progress.value = progress.innerHTML = complete;
             }
-          }, false);
-          return xhr;
-        },
-        type: 'POST',
-        url: "/upload/",
-        data: formdata,
-        success: function(data){
-          progress.value = progress.innerHTML = 100;
-          alert(data);
+          }
         }
-      });
-    }
+
+        xhr.onreadystatechange = function(){
+          if ((xhr.readyState == 4) || (xhr.status == 200)){
+            alert(xhr.responseText);
+          }
+        }
+
+        xhr.send(formData);
+      }
+      // // now post a new XHR request
+      // $.post('/upload/', formData)
+      //   .done(function(data){
+      //     alert(data);
+      //   });
   }
 
   if (tests.dnd) {
@@ -157,3 +111,4 @@ $(document).ready(function() {
     };
   }
 });
+
