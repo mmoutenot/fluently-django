00d4051bd2e9e9d9d6cc2a21ea41bedfa0466a54
Marshall <mmoutenot@gmail.com>
2013-04-30 20:53:45 -0400

    add homepage and login views

--- apps/face/views.py
+++ apps/face/views.py
@@ -1,1 +1,26 @@
-# Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+
+
+def main(request):
+  return render(request, 'face/login.html')
+
+def login_user(request):
+  email = password = ''
+  if request.POST:
+    email = request.POST.get('login_email')
+    password = request.POST.get('login_password')
+    print 'trying to log in with ', email, password
+
+    user = authenticate(username=email, password=password)
+    if user is not None:
+      if user.is_active:
+        login(request, user)
+        print 'user active and logged in!'
+        return redirect('/space/')
+      else:
+       print 'user not active'
+    else:
+      print 'invalid user'
+  return redirect('/')
+
