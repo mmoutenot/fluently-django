d9e0e4bb96dbcc9aa3de69247217ea4a5b4bf995
Marshall <mmoutenot@gmail.com>
2013-05-03 19:24:40 -0400

    add chat socket handlers

--- apps/space/events.py
+++ apps/space/events.py
@@ -10,7 +10,20 @@ def message(request, socket, context, message):
 
   # space = get_object_or_404(Space, message['space_id'])
   print 'message received', message
+  user = request.user
 
   if message['action'] == 'loaded_doc':
     croco_session = Crocodoc.generate_session_id(strip_tags(message['croco_uuid']))
     socket.broadcast_channel({'action':'load_doc', 'croco_session':croco_session})
+
+  elif message['action'] == 'start':
+    context["user"] = user
+    socket.broadcast_channel({"action": "join", "name": user.get_full_name(), "id": user.id})
+
+  elif message["action"] == "message":
+    print 'received message on server with content: ', message["message"]
+    message["message"] = strip_tags(message["message"])
+    message["name"] = user.get_full_name()
+    socket.send_and_broadcast_channel(message)
+
+
