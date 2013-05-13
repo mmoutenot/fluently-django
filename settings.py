b2ac319ea6def87a36f678721b08b269020a7782
Marshall <mmoutenot@gmail.com>
2013-05-13 01:56:19 -0400

    master now represents production. NO MORE COMMITS DIRECTLY INTO MASTER

--- settings.py
+++ settings.py
@@ -24,8 +24,8 @@ DATABASES = {
     'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
     'NAME': 'fluently',            # Or path to database file if using sqlite3.
     'USER': 'fluently',            # Not used with sqlite3.
-    'PASSWORD': 'fluently',          # Not used with sqlite3.
-    'HOST': '',            # Set to empty string for localhost. Not used with sqlite3.
+    'PASSWORD': 'password',          # Not used with sqlite3.
+    'HOST': '0.0.0.0',            # Set to empty string for localhost. Not used with sqlite3.
     'PORT': '',            # Set to empty string for default. Not used with sqlite3.
   }
 }
