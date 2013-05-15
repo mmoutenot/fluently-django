04277cbcb98408dca9f592813dfeed277263c8e4
Marshall <mmoutenot@gmail.com>
2013-05-15 01:19:32 -0400

    Add a route to handle the registration ajax calls

--- apps/face/urls.py
+++ apps/face/urls.py
@@ -5,5 +5,6 @@ from django.conf.urls.defaults import patterns, include, url
 urlpatterns = patterns('',
                        url(r'^$', 'apps.face.views.main'),
                        url(r'^login/', 'apps.face.views.login_user'),
-                       url(r'^register/', 'apps.face.views.register_user'),
+                       url(r'^register/$', 'apps.face.views.register_user'),
+                       url(r'^register/account_handler', 'apps.face.views.register_account_handler'),
 )
