2888e07be14fb3ce0138b24cbfb5314536cb0cea
Dylan Portelance <dylanjportelance@Dylans-MacBook-Pro.local>
2013-06-06 14:56:01 -0400

    created userprofile class one to one with users

--- apps/face/models.py
+++ apps/face/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfile(models.Model):
+  user = models.OneToOneField(User, primary_key=True)
+  certification = models.CharField(max_length=50)
+  education = models.CharField(max_length=50)
+  membership = models.CharField(max_length=50)
+  experience_specialties = models.CharField(max_length=200)
+
+User.profile = property(lambda u: UserProfile.objects.get_or_create(user=u)[0])
