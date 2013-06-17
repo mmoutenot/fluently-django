be740ed5ddbf02f2a562681fa633a7f63606415c
Dylan Portelance <dylanjportelance@Dylans-MacBook-Pro.local>
2013-06-17 11:43:43 -0400

    format change in post request

--- static/javascripts/register.js
+++ static/javascripts/register.js
@@ -132,7 +132,7 @@ $(document).ready(function() {
         data = {
           stage:                 stage,
           email:                 email,
-          admin_request:         $('#account-admin-request').prop('checked');
+          adminRequest:         $('#account-admin-request').prop('checked');
           csrfmiddlewaretoken:   csrf_token
         };
       }
