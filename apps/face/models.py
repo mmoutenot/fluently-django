477ff7d701e9bce7aeb5496a626b79db7148dbc8
Dylan Portelance <dylanjportelance@Dylans-MacBook-Pro.local>
2013-06-20 15:12:15 -0400

    changed form fields

--- apps/face/models.py
+++ apps/face/models.py
@@ -4,10 +4,13 @@ from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
   user = models.OneToOneField(User, primary_key=True)
-  admin = models.BooleanField(default=False)
-  company = models.CharField(max_length=36)
+  phone = models.CharField(max_length=36)
+  name = models.CharField(max_length=64)
+  state = models.CharField(max_length=32)
+  specialties = models.CharField(max_length=128)
   join_id = models.CharField(max_length=36, unique=True)
   confirmed = models.BooleanField(default=False)
+  emailed = models.BooleanField(default=False)
 
   def __str__(self):
     return "%s's profile" % self.user
