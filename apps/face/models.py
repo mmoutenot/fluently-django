df6475b5bc759e89766b8f62c88ac4e5e1c03cef
Dylan Portelance <dylanjportelance@Dylans-MacBook-Pro.local>
2013-07-01 14:18:32 -0400

    student needs

--- apps/face/models.py
+++ apps/face/models.py
@@ -3,21 +3,29 @@ from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
-  user = models.OneToOneField(User, primary_key=True)
-  phone = models.CharField(max_length=36)
-  name = models.CharField(max_length=64)
-  state = models.CharField(max_length=32)
-  specialties = models.CharField(max_length=512)
-  join_id = models.CharField(max_length=36, unique=True)
-  pic_url = models.CharField(max_length=512, unique=True)
-  confirmed = models.BooleanField(default=False)
-  emailed = models.BooleanField(default=False)
 
-  def __str__(self):
-    return "%s's profile" % self.user
+    USER_TYPE_CHOICES = (
+        ('P', 'Provider'),
+        ('S', 'Student'),
+    )
+
+    user = models.OneToOneField(User, primary_key=True)
+    user_type = models.CharField(max_length=1, choices=USER_TYPE_CHOICES)
+    phone = models.CharField(max_length=36)
+    name = models.CharField(max_length=64)
+    location = models.CharField(max_length=32)
+    specialties = models.CharField(max_length=512)
+    needs = models.CharField(max_length=512)
+    join_id = models.CharField(max_length=36, unique=True)
+    pic_url = models.CharField(max_length=512, unique=True)
+    confirmed = models.BooleanField(default=False)
+    emailed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return "%s's profile" % self.user
 
 def create_profile(sender, instance, created, **kwargs):
-  if created:
-    profile, created = UserProfile.objects.get_or_create(user=instance)
+    if created:
+        profile, created = UserProfile.objects.get_or_create(user=instance)
 
 post_save.connect(create_profile, sender=User)
