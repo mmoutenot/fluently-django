28d0e5dc903b601559ec166d18b6fbb292b2dda4
Dylan Portelance <dylanjportelance@Dylans-MacBook-Pro.local>
2013-07-12 13:17:50 -0400

    multiple signups on student modal

--- apps/face/views.py
+++ apps/face/views.py
@@ -11,6 +11,8 @@ from utils import mandrill_template
 import json
 import requests
 import uuid
+import string
+from random import choice
 
 # Display landing page
 def main(request):
@@ -19,6 +21,14 @@ def main(request):
 # Display signin page  
 def signin(request):
     return render(request, 'face/signin.html')
+
+# Display about page  
+def about(request):
+    return render(request, 'face/about.html')
+
+# Display splash page
+def splash(request):
+    return render(request, 'face/splash.html')
 
 # Check validity of signin info from client via request
 # Redirect to space or send error
@@ -55,62 +65,66 @@ def register(request):
 def student_account_handler(request):
     response_json = {"status": "fail"}
     if request.POST:
-        print "POST == true"
         email = request.POST.get('email', "")
         name = request.POST.get('name', "")
         location = request.POST.get('loc', "")
         needs = request.POST.get('needs', "")
+        print email
+        print name
         if User.objects.filter(username=email).count():
-            print "emailed == true"
+            print User.objects.filter(username=email)[0].userprofile.emailed
             response_json = {
                 "status": "success",
                 "emailed": User.objects.filter(username=email)[0]
                            .userprofile.emailed
             }
         else:
-            u, created = User.objects.get_or_create(username=email)
-            if created:
-                print "created "
-                print u
-                u.set_unusable_password()
-                u.userprofile.user_type = 'S'
-                u.userprofile.join_id = str(uuid.uuid1())
-                u.userprofile.name = name
-                u.userprofile.location = location 
-                u.userprofile.needs = needs
-                u.userprofile.emailed = True
-                u.save()
-                u.userprofile.save()
-                mandrill_url = ("https://mandrillapp.com/api/1.0/messages/"
-                                "send-template.json")
-                template_content_ceo = [
-                    { "name": "name", "content": name },
-                    { "name": "email", "content": email },
-                    { "name": "location", "content": location },
-                    { "name": "needs", "content": needs }]
-                mandrill_template_ceo = mandrill_template("student-request", 
-                                                          template_content_ceo, 
-                                                          "dylan@fluentlynow.com", 
-                                                          "Jack McDermott", 
-                                                          "student-request")
-                requests.post(mandrill_url, data=mandrill_template_ceo)
-                template_content_user = [
-                    {"name": "name", "content": name }]
-                mandrill_template_user = mandrill_template("student-app-received",
-                                                       template_content_user,
-                                                       email,
-                                                       name,
-                                                       "student-app-received")
-                requests.post(mandrill_url, data=mandrill_template_user) 
-                response_json = { 
-                    "status": "success",
-                    "emailed": False
-                }
+            u = User.objects.create(username=email)
+            u.set_unusable_password()
+            u.userprofile.user_type = 'S'
+            u.userprofile.join_id = str(uuid.uuid1())
+            u.userprofile.name = name
+            u.userprofile.location = location 
+            u.userprofile.needs = needs
+            u.userprofile.emailed = True
+            alphnum = string.ascii_uppercase + string.digits
+            u.userprofile.user_url = ''.join(choice(alphnum) for x in range(6))
+            u.save()
+            u.userprofile.save()
+            mandrill_url = ("https://mandrillapp.com/api/1.0/messages/"
+                            "send-template.json")
+            template_content_ceo = [
+                { "name": "name", "content": name },
+                { "name": "email", "content": email },
+                { "name": "location", "content": location },
+                { "name": "needs", "content": needs }]
+            mandrill_template_ceo = mandrill_template("student-request", 
+                                                      template_content_ceo, 
+                                                      "dylan@fluentlynow.com", 
+                                                      "Jack McDermott", 
+                                                      "student-request")
+            requests.post(mandrill_url, data=mandrill_template_ceo)
+            template_content_user = [
+                {"name": "name", "content": name }]
+            mandrill_template_user = mandrill_template("student-app-received",
+                                                   template_content_user,
+                                                   email,
+                                                   name,
+                                                   "student-app-received")
+            requests.post(mandrill_url, data=mandrill_template_user) 
+            response_json = { 
+                "status": "success",
+                "emailed": False
+            }
     return HttpResponse(json.dumps(response_json), mimetype="application/json") 
 
 # Display register student page
 def register_student(request):
     return render(request, 'face/register_student_modal.html')
+
+# Display register student blocks
+def register_student_blocks(request):
+    return render(request, 'face/register_student_blocks.html')
 
 # Display register blocks
 def register_blocks(request):
@@ -131,6 +145,7 @@ def confirm_user(request):
     if request.POST:
         join_id = request.POST.get('join_id', "")
         try: 
+            print "here"
             u = UserProfile.objects.filter(join_id=join_id)[0].user
             email = u.username
             confirmed = u.userprofile.confirmed
@@ -138,6 +153,7 @@ def confirm_user(request):
                 "status": "success", 
                 "confirmed": confirmed, 
                 "email": email }
+            print response_json
             return HttpResponse(json.dumps(response_json), 
                    mimetype="application/json") 
         except:  
@@ -192,7 +208,8 @@ def register_account_handler(request):
     response_json = {"status": "fail"}
     if request.POST:
         print request.POST
-        name = request.POST.get('name', "")
+        firstName = request.POST.get('firstName', "")
+        lastName = request.POST.get('lastName', "")
         email = request.POST.get('email', "")
         phone = request.POST.get('phone', "")
         location = request.POST.get('loc', "")
@@ -201,8 +218,11 @@ def register_account_handler(request):
         print "created"
         u.set_unusable_password()
         u.userprofile.user_type = 'P'
+        alphnum = string.ascii_uppercase + string.digits
+        u.userprofile.user_url = ''.join(choice(alphnum) for x in range(6))
         u.userprofile.join_id = str(uuid.uuid1())
-        u.userprofile.name = name
+        u.userprofile.first_name = firstName
+        u.userprofile.last_name = lastName
         u.userprofile.phone = phone
         u.userprofile.location = location 
         u.userprofile.specialties = specialties
@@ -213,23 +233,24 @@ def register_account_handler(request):
         mandrill_url = ("https://mandrillapp.com/api/1.0/messages/"
                        "send-template.json")
         template_content_ceo = [
-            { "name": "name", "content": name },
+            { "name": "firstName", "content": firstName },
+            { "name": "lastName", "content": lastName },
             { "name": "email", "content": email },
             { "name": "phone", "content": phone },
             { "name": "location", "content": location },
             { "name": "specialties", "content": specialties }]
         mandrill_template_ceo = mandrill_template("provider-request", 
                                                   template_content_ceo, 
-                                                  "dylan@fluentlynow.com", 
+                                                  "team@fluentlynow.com", 
                                                   "Jack McDermott", 
                                                   "provider-request")
         requests.post(mandrill_url, data=mandrill_template_ceo)
         template_content_user = [
-            {"name": "name", "content": name }]
+            {"name": "firstName", "content": firstName }]
         mandrill_template_user = mandrill_template("provider-app-received",
                                                    template_content_user,
                                                    email,
-                                                   name,
+                                                   firstName,
                                                    "provider-app-received")
         requests.post(mandrill_url, data=mandrill_template_user) 
         response_json = {"status": "success"}
