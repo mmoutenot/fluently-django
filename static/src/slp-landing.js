cc069b39dcf67aea1aef192d6e50bfa06ffc5d20
jackrmcdermott@gmail.com <jackrmcdermott@gmail.com>
2013-08-08 11:11:12 -0400

    style fixes + JS button click

--- static/src/slp-landing.js
+++ static/src/slp-landing.js
@@ -13,3 +13,14 @@ setInterval(function () {
   }
   $('#complete-free-profile').attr('href', url);
 }, observeInterval);
+
+$(document).ready(function () {
+	$('.ebook-button').click(function () {
+    	$('#sent-success').css('visibility', 'visible');
+    	setTimeout(function () {
+	    	$('#sent-success').css('visibility', 'hidden');
+	    }, 2000); 
+    }
+} 
+    
+    
