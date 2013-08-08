3b5f94269ad5550b5ae7ed3191d44758a3f2cd3a
Dylan Portelance <dylanjportelance@Dylans-MacBook-Pro.local>
2013-08-08 17:05:55 -0400

    tweaked front end for ebook requests

--- static/src/how-it-works.js
+++ static/src/how-it-works.js
@@ -2,22 +2,24 @@ $(document).ready(function () {
 	
   $('.ebook-button').click(function () {
 
-    $.ajax({
-      type: 'post',
-      dataType: 'json',
-      url: '/ebook/',
-      data: {
-        recipientType: 'parent',
-        email: $('#signin_email').val(),
-        csrfmiddlewaretoken: csrf_token 
-      },
-      success: function (dataJSON) {
-        $('#sent-success').css('visibility', 'visible');
-        setTimeout(function () {
-          $('#sent-success').css('visibility', 'hidden');
-        }, 2000); 
-      }
-    });
+    if (!$('#signin_email').val()) {
+      $.ajax({
+        type: 'post',
+        dataType: 'json',
+        url: '/ebook/',
+        data: {
+          recipientType: 'parent',
+          email: $('#signin_email').val(),
+          csrfmiddlewaretoken: csrf_token 
+        },
+        success: function (dataJSON) {
+          $('#sent-success').css('visibility', 'visible');
+          setTimeout(function () {
+            $('#sent-success').css('visibility', 'hidden');
+          }, 4000); 
+        }
+      });
+    }
 
   });
 
--- static/src/slp-landing.js
+++ static/src/slp-landing.js
@@ -17,23 +17,24 @@ $(document).ready(function () {
   }, observeInterval);
 
 	$('.ebook-button').click(function () {
-
-    $.ajax({
-      type: 'post',
-      dataType: 'json',
-      url: '/ebook/',
-      data: {
-        recipientType: 'slp',
-        email: $('#signin_email').val(),
-        csrfmiddlewaretoken: csrf_token 
-      },
-      success: function (dataJSON) {
-        $('#sent-success').css('visibility', 'visible');
-        setTimeout(function () {
-          $('#sent-success').css('visibility', 'hidden');
-        }, 2000); 
-      }
-    });
+    if (!$('#signin_email').val() {
+      $.ajax({
+        type: 'post',
+        dataType: 'json',
+        url: '/ebook/',
+        data: {
+          recipientType: 'slp',
+          email: $('#signin_email').val(),
+          csrfmiddlewaretoken: csrf_token 
+        },
+        success: function (dataJSON) {
+          $('#sent-success').css('visibility', 'visible');
+          setTimeout(function () {
+            $('#sent-success').css('visibility', 'hidden');
+          }, 4000); 
+        }
+      });
+    }
 
   });
 
